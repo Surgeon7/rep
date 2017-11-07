a01aa81c98892ec96f3d6dedb5422e746da7cfe8
Yura <lolkekcheburek7@bk.ru>
2017-11-07 12:40:02 +0300

    Dobavil task1

---
+++ task1/func1.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#ifndef FUNC1
+#define FUNC1
+
+/* func1 выводит строку "Lol"
+ */
+
+void func1();
+
+#endif
---
+++ task1/func2.c
@@ -0,0 +1,6 @@
+#include "func2.h"
+
+void func2 ()
+{
+    printf ("Kek\n");
+}
